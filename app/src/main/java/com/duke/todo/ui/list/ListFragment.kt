c57ffb49d01a0bc3fe22f658c844ce13212fb70e
Mojtaba Eshaghi <mr.mojtaba47@gmail.com>
2021-02-13 22:20:35 +0330

    add option menu

--- app/src/main/java/com/duke/todo/ui/list/ListFragment.kt
+++ app/src/main/java/com/duke/todo/ui/list/ListFragment.kt
@@ -1,9 +1,7 @@
 package com.duke.todo.ui.list
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.duke.todo.R
@@ -30,9 +28,16 @@ class ListFragment : Fragment() {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
 
-
+        setHasOptionsMenu(true)
 
         return view
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_frgment_menu,menu)
+        super.onCreateOptionsMenu(menu, inflater)
+
     }
 
     override fun onDestroy() {
