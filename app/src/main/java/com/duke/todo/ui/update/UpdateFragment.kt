a4804e68d3c692c56e990aacbdeec52b1e067c89
Mojtaba Eshaghi <mr.mojtaba47@gmail.com>
2021-02-13 22:44:16 +0330

    add option menu to update fragment change fragment layout

--- app/src/main/java/com/duke/todo/ui/update/UpdateFragment.kt
+++ app/src/main/java/com/duke/todo/ui/update/UpdateFragment.kt
@@ -1,21 +1,32 @@
 package com.duke.todo.ui.update
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.duke.todo.R
+import com.duke.todo.databinding.FragmentUpdateBinding
 
 
 class UpdateFragment : Fragment() {
+    private var _bindig: FragmentUpdateBinding? = null
+    private val binding get() = _bindig!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update, container, false)
+        _bindig = FragmentUpdateBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+
+        setHasOptionsMenu(true)
+
+        return view
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_fragment_meun, menu)
     }
 
 
